d3bf88ba07e94328155d931cfdda15047956bb52
zhaomeng.teddy <zhaomeng.teddy@bytedance.com>
2019-06-21 20:48:20 +0800

    创建工程

---
+++ app/src/main/java/com/example/fund/AddActivity.kt
@@ -0,0 +1,15 @@
+package com.example.fund
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ *  Created by zhaomeng on 2019/6/21
+ */
+class AddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_add)
+    }
+}
